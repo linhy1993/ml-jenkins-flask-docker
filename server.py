bb74b4795fb60be95d82a3937171fadd8796b8f5
linhy1993 <linhongyi1110@gmail.com>
2020-10-09 16:41:18 -0400

    modify

--- server.py
+++ server.py
@@ -16,16 +16,6 @@ def predict():
     result = model.predict(data['feature'])
     return jsonify(result[0].tolist())
 
-# webhook
-@app.route('/github_webhook', methods=['POST'])
-def rebuild():
-    print('new commits to github repository')
-    # subprocess.run can just deal with the first change
-    # since it stuck in it, use Popen instead
-    # subprocess.run(['sh', 'build_and_run.sh'])
-    subprocess.Popen(['sh', 'build_and_run.sh'])
-    return jsonify('got it')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
