09cf8065720ad421192e4bb1eeb6d0cbef947392
linhy1993 <linhongyi1110@gmail.com>
2020-10-09 15:39:38 -0400

    remove hook

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import pickle
+import subprocess
 
 from flask import Flask, request, jsonify
 
@@ -14,6 +15,16 @@ def predict():
     data = request.get_json(force=True)
     result = model.predict(data['feature'])
     return jsonify(result[0].tolist())
+
+
+@app.route('/github_webhook', methods=['POST'])
+def rebuild():
+    print('new commits to github repository')
+    # subprocess.run can just deal with the first change
+    # since it stuck in it, use Popen instead
+    # subprocess.run(['sh', 'build_and_run.sh'])
+    subprocess.Popen(['sh', 'build_and_run.sh'])
+    return jsonify('got it')
 
 
 if __name__ == '__main__':
